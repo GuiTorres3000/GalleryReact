6c8ef28662c5fd36dfb51f9e0957e0a358b6b52f
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-09-04 23:13:24 -0300

    chore: base project gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from "./components/button";
+import ButtonIcon from "./components/button-icon";
+import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
+import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
+import Badge from "./components/badge";
+import Alert from "./components/alert";
+import Divider from "./components/divider";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+	return (
+		<div className="grid gap-7 p-6">
+			<div className="flex gap-3">
+				<Button>Button</Button>
+				<Button variant="secondary">Button</Button>
+				<Button disabled>Button</Button>
+				<Button handling>Loading</Button>
+				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
+				<Button variant="ghost" size="sm">
+					Button
+				</Button>
+				<Button variant="primary" size="sm">
+					Button
+				</Button>
+			</div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			<div className="flex gap-3">
+				<ButtonIcon icon={ChevronLeftIcon} />
+				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
+			</div>
+
+			<div className="flex gap-3">
+				<Badge>Todos</Badge>
+				<Badge>Natureza</Badge>
+				<Badge>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+			</div>
+
+			<div>
+				<Alert>
+					Tamanho máximo: 50MB
+					<br />
+					Você pode selecionar arquivos em PNG, JPG, JPEG ou WEBP
+				</Alert>
+			</div>
+
+			<div>
+				<Divider />
+			</div>
+		</div>
+	);
 }
-
-export default App
