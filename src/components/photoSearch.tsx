d1f99b3fdaf4661755b9ff415c8e951dad4718bd
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-09-08 21:49:44 -0300

    feat: photoSeach with debounce

--- src/components/photoSearch.tsx
+++ src/components/photoSearch.tsx
@@ -1,8 +1,25 @@
 import InputText from "./inputText";
 import SearchIcon from "../assets/icons/search.svg?react";
+import { useCallback, useState } from "react";
+import { debounce } from "../helpers/utils";
 
 export default function PhotoSearch() {
+
+  const [inputValue, setInputValue] = useState("");
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSetValue = useCallback(
+    debounce((value: string) => console.log("Valor com debounce", value), 300), []
+  );
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setInputValue(value);
+    debouncedSetValue(value);
+  }
+
   return (
-      <InputText icon={SearchIcon} placeholder="Buscar fotos" className="flex-1"/>
+      <InputText icon={SearchIcon} placeholder="Buscar fotos" className="flex-1"
+      value={inputValue} onChange={handleInputChange}/>
   )
 }
---
+++ src/helpers/utils.ts
@@ -0,0 +1,22 @@
+// Função debounce serve para controlar a frequência de execução de uma função
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+) {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return function (...args: Parameters<T>): void {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(later, wait);
+  };
+}
