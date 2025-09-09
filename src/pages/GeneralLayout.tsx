3deeb36ad9f8a7fb04d6a92786c9356912b50b13
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-09-08 23:01:47 -0300

    feat: mainContent component

--- src/pages/GeneralLayout.tsx
+++ src/pages/GeneralLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router";
 import MainHeader from "../components/layout/mainHeader";
+import MainContent from "../components/layout/mainContent";
 
 export default function GeneralLayout() {
       return (
             <div>
                   <MainHeader className="mt-9"/>
+                  <MainContent />
                   <Outlet />
 
             </div>           
