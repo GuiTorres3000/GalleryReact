4e968bef7dba3b24f9f12cb68f23a208eecf12f6
GuiTorres3000 <guilhermetorres3000@gmail.com>
2025-09-13 23:07:14 -0300

    chore: config react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import Layout from "./pages/Layout";
 import GeneralLayout from "./pages/GeneralLayout";
 import Home from "./pages/Home";
 import PhotoDetail from "./pages/PhotoDetail";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<GeneralLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/photo/:id" element={<PhotoDetail />} />
-          <Route path="/components" element={<Layout />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<GeneralLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/photo/:id" element={<PhotoDetail />} />
+            <Route path="/components" element={<Layout />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
